424e12232900b54bcc78884936ae9c8ded9282e7
Raqib Ali <raqib@inventystudio.com>
2025-09-04 22:48:41 +0530

    Added routes in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,32 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import './App.css'
+import FetchConfigPage from './pages/FetchConfigPage'
+import UpdateConfigPage from './pages/UpdateConfigPage'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BrowserRouter>
+
+        <div>
+          <nav style={{
+            display: "flex",
+            justifyContent: "space-between"
+          }}>
+            <Link style={{ fontSize: "20px" }} to="/">Fetch Config</Link>
+            <Link style={{ fontSize: "20px" }} to="/update">Update Remark</Link>
+          </nav>
+        </div>
+
+        <Routes>
+          <Route path='/' element={<FetchConfigPage />}></Route>
+          <Route path='/update' element={<UpdateConfigPage />} />
+        </Routes>
+
+      </BrowserRouter>
     </>
   )
 }
