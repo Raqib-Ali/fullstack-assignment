0b4efd07432a0b8e310eb378fe9368a009e4b79e
Raqib Ali <raqib@inventystudio.com>
2025-09-04 14:00:02 +0530

    Added config routes

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,14 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import ConfigRoutes from "./routes/configuration.routes";
 
 dotenv.config();
 const app = express();
 app.use(express.json());
+
+// Routes
+app.use('/api/configurations', ConfigRoutes);
 
 const PORT = process.env.PORT || 8080;
 const MONGO_URI = process.env.MONGO_URI;
